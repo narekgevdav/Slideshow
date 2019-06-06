e975ffcd24758bfdf2985740b82b16152e828870
narekgevdav <49719556+narekgevdav@users.noreply.github.com>
2019-06-07 02:56:10 +0400

    Add files via upload

--- sctipt.js
+++ sctipt.js
@@ -1,15 +1,17 @@
 let imagelist = ["images/img1.jpg","images/img2.jpg","images/img3.jpg","images/img4.jpg","images/img5.jpg"]
 let start = document.getElementById("start")
 let stop = document.getElementById("stop")
+let image = document.getElementById("images")
 start.addEventListener("click", slidshow)
+
+
 
 function slidshow(){
     start.style.visibility = "hidden"
     stop.style.visibility = "visible"
     stop.addEventListener("click", stopSlide)    
     let imgNumber = 0
-    let div = document.getElementById("slidshow")
-    let image = document.createElement("img")
+    let div = document.getElementById("slidshow")      
     image.src = imagelist[0]
     div.appendChild(image)
     let myInterval = setInterval(changeImage,3000)
@@ -20,17 +22,21 @@ function slidshow(){
         } else{
             
             clearInterval(myInterval)
-            div.removeChild(image)
+            image.src = ""
+            start.style.visibility = "visible"
+            stop.style.visibility = "hidden" 
             
         }
 
     }
     function stopSlide(){
         clearInterval(myInterval)
-          div.removeChild(image)
-          start.style.visibility = "visible"
-    stop.style.visibility = "hidden" 
+        image.src = ""
+       
+        start.style.visibility = "visible"
+        stop.style.visibility = "hidden" 
     }
+   
     
     
 
